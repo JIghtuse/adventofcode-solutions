d588ec6222dca48362227a4d565f8ea1d17fd4d9
Boris Egorov <egorov@linux.com>
2015-12-08 11:35:32 +0600

    day8: Python solution

---
+++ day8/esc.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+import re
+
+lines = []
+with open("input") as filep:
+    lines = filep.readlines()
+
+orig = 0
+for line in lines:
+    line = line.strip()
+    orig += len(line)
+
+esc = 0
+for line in lines:
+    line = line.strip()
+    line = line[1:-1]
+    line = re.sub(r'\\x[0-9a-f]{2}', 'Z', line)
+    line = re.sub(r'\\"', '"', line)
+    line = re.sub(r'\\\\', 'B', line)
+    esc += len(line)
+
+unesc = 0
+for line in lines:
+    line = line.strip()
+    line = re.sub(r'\\', '\\\\\\\\', line)
+    line = re.sub(r'"', '\\"', line)
+    line = '"' + line + '"'
+    unesc += len(line)
+
+print(orig-esc)
+print(unesc-orig)
+
+#    print("line: [{}] {}".format(len(line), line))
