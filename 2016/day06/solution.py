f15f99f335ccd909b4ee4b64e947157a97a26028
Boris Egorov <egorov@linux.com>
2016-12-06 13:44:54 +0700

    2016/day06: Simplify code with zip()

--- 2016/day06/solution.py
+++ 2016/day06/solution.py
@@ -9,14 +9,12 @@ def read_data(fname):
 
 def solve_first(fname):
     data = read_data(fname)
-    return ''.join((str(Counter(d[i] for d in data).most_common()[0][0])
-                   for i in range(len(data[0]))))
+    return ''.join((Counter(d).most_common()[0][0]) for d in zip(*data))
 
 
 def solve_second(fname):
     data = read_data(fname)
-    return ''.join((str(Counter(d[i] for d in data).most_common()[-1][0])
-                   for i in range(len(data[0]))))
+    return ''.join((Counter(d).most_common()[-1][0]) for d in zip(*data))
 
 
 print(solve_first("input.txt"))
