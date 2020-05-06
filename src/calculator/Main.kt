1037bdb6b0108384b6f44f0fe482f227c496b406
Boris Egorov <egorov@linux.com>
2020-05-06 22:02:51 +0700

    Stage 6/8: Variables

--- src/calculator/Main.kt
+++ src/calculator/Main.kt
@@ -1,38 +1,79 @@
 package calculator
 
+import java.lang.NumberFormatException
+
 fun getTokens(line: String) = line.split(" ").filter { it.isNotEmpty() }
 
-fun isPlus(token: String): Boolean {
+// operations
+
+fun isPlusOperation(token: String): Boolean {
     val isPlusByMinuses = token.length % 2 == 0 && token.all { it == '-' }
     val isPlusChars = token.all { it == '+' }
     return isPlusChars || isPlusByMinuses
 }
 
-fun isMinus(token: String): Boolean = token.all { it == '-' }
+fun isMinusOperation(token: String): Boolean = token.all { it == '-' }
 
 fun getOperation(token: String): Int.(Int) -> Int {
-    if (isPlus(token)) {
+    if (isPlusOperation(token)) {
         return Int::plus
-    } else if (isMinus(token)) {
+    } else if (isMinusOperation(token)) {
         return Int::minus
     } else {
         throw Exception("Invalid expression")
     }
 }
 
-fun parseAndCalculate(line: String): Int {
+// variables
+
+fun isAssignment(s: String) = '=' in s
+
+fun isValidVariableName(s: String) = s.all{ it.isLetter() }
+
+fun parseAndAssign(line: String, variables: MutableMap<String, Int>) {
+    val parts = line.split('=')
+    if (parts.size != 2) throw Exception("Invalid assignment")
+
+    val namePart = parts[0].trim()
+    if (!isValidVariableName(namePart)) throw Exception("Invalid identifier")
+
+    variables[namePart] = getValue(variables, parts[1].trim(), "Invalid assignment")
+}
+
+fun getVariableValue(activeVariables: MutableMap<String, Int>, variableName: String): Int {
+    val rightHandSideValue = activeVariables[variableName]
+    if (rightHandSideValue != null) {
+        return rightHandSideValue
+    } else {
+        throw Exception("Unknown variable")
+    }
+}
+
+fun getValue(activeVariables: MutableMap<String, Int>, expression: String, invalidNameMessage: String): Int {
+    return if (isValidVariableName(expression)) {
+        getVariableValue(activeVariables, expression)
+    } else if (expression.any { it.isLetter() }) {
+        throw Exception(invalidNameMessage)
+    } else {
+        expression.toInt()
+    }
+}
+
+// calculating single expression
+
+fun parseAndCalculate(line: String, activeVariables: MutableMap<String, Int>): Int {
     val tokens = getTokens(line)
 
     var result = 0
     var activeOperation: Int.(Int) -> Int = Int::plus
 
     for (tokenIndex in tokens.indices) {
-        val isNumberIndex = tokenIndex % 2 == 0
+        val isValueIndex = tokenIndex % 2 == 0
         val token = tokens[tokenIndex]
 
-        if (isNumberIndex) {
-            val n = token.toInt()
-            result = activeOperation(result, n)
+        if (isValueIndex) {
+            val value = getValue(activeVariables, token, "Invalid expression")
+            result = activeOperation(result, value)
         } else {
             activeOperation = getOperation(token)
         }
@@ -46,6 +87,8 @@ fun printHelp() {
 }
 
 fun main() {
+    val activeVariables = mutableMapOf<String, Int>()
+
     while (true) {
         val line = readLine()!!
 
@@ -58,9 +101,16 @@ fun main() {
             println("Unknown command")
         } else {
             try {
-                println(parseAndCalculate(line))
-            } catch (_: java.lang.Exception) {
+                if (isAssignment(line)) {
+                    parseAndAssign(line, activeVariables)
+                } else {
+                    val result = parseAndCalculate(line, activeVariables)
+                    println(result)
+                }
+            } catch(e: NumberFormatException) {
                 println("Invalid expression")
+            } catch (e: java.lang.Exception) {
+                println(e.message)
             }
         }
     }
