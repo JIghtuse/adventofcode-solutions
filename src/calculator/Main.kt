0b96cfbac814fd9f42204e75717a5292ed4698af
Boris Egorov <egorov@linux.com>
2020-05-06 22:01:54 +0700

    Stage 4/8: Add subtractions

--- src/calculator/Main.kt
+++ src/calculator/Main.kt
@@ -2,6 +2,42 @@ package calculator
 
 import java.util.Scanner
 
+fun getTokens(line: String) = line.split(" ").filter { it.isNotEmpty() }
+
+fun isPlus(token: String): Boolean {
+    val isPlusByMinuses = token.length % 2 == 0 && token.all { it == '-' }
+    val isPlusChars = token.all { it == '+' }
+    return isPlusChars || isPlusByMinuses
+}
+
+fun getOperation(token: String): Int.(Int) -> Int {
+    return if (isPlus(token)) Int::plus else Int::minus
+}
+
+fun parseAndCalculate(line: String): Int {
+    val tokens = getTokens(line)
+
+    var result = 0
+    var activeOperation: Int.(Int) -> Int = Int::plus
+
+    for (tokenIndex in tokens.indices) {
+        val isNumberIndex = tokenIndex % 2 == 0
+        val token = tokens[tokenIndex]
+        if (isNumberIndex) {
+            val n = token.toInt()
+            result = activeOperation(result, n)
+        } else {
+            activeOperation = getOperation(token)
+        }
+    }
+    return result
+}
+
+fun printHelp() {
+    println("The program calculates simple operations on numbers")
+    println("You can input numbers with + and - between them which you want to add/subtract")
+}
+
 fun main() {
     val scanner = Scanner(System.`in`)
 
@@ -12,10 +48,9 @@ fun main() {
 
         if (line == "/exit") break
 
-        if (line == "/help") println("The program calculates the sum of numbers")
+        if (line == "/help") printHelp()
 
-        val numbers = line.split(" ")
-        println(numbers.map { it.toInt() }.sum())
+        println(parseAndCalculate(line))
     }
 
     println("Bye!")
