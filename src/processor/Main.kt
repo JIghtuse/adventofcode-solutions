723891addd1a346956b79b93acf31bb8ce9aff6f
Егоров Борис Владимирович <b.egorov@2gis.ru>
2020-05-09 12:20:03 +0700

    Implement Matrix and Matrix.plus

---
+++ src/processor/Main.kt
@@ -0,0 +1,75 @@
+package processor
+
+fun toIntList(s: String): List<Int> {
+    return s.split(" ").filter { it.isNotEmpty() }.map { it.toInt() }
+}
+
+fun toArrayList(xs: List<Int>): ArrayList<Int> {
+    val arrayList = arrayListOf<Int>()
+
+    for (item in xs) {
+        arrayList.add(item)
+    }
+
+    return arrayList
+}
+
+class Matrix(val items: ArrayList<ArrayList<Int>>) {
+    val rows: Int = items.size
+    val columns: Int = if (items.isEmpty()) 0 else items[0].size
+
+    operator fun plus(other: Matrix): Matrix {
+        if (rows != other.rows) throw Exception("Could not sum matrix of different dimensions (rows)")
+        if (columns != other.columns) throw Exception("Could not sum matrix of different dimensions (columns)")
+
+        val sumMatrixItems = arrayListOf<ArrayList<Int>>()
+        repeat(rows) {
+            val row = arrayListOf<Int>()
+            repeat(columns) { jt ->
+                row.add(getItem(it, jt) + other.getItem(it, jt))
+            }
+            sumMatrixItems.add(row)
+        }
+
+        return Matrix(sumMatrixItems)
+    }
+
+    override fun toString(): String {
+        return items.joinToString("\n") { it.joinToString(" ") }
+    }
+
+    private fun getItem(row: Int, col: Int): Int {
+        if (row >= rows) throw Exception("Asked for row $row, matrix has only $rows rows.")
+        if (col >= columns) throw Exception("Asked for column $col, matrix has only $columns columns.")
+        return items[row][col]
+    }
+
+    companion object {
+        fun readFromStdin(): Matrix {
+            val dimensions = toIntList(readLine()!!)
+            assert(dimensions.size == 2)
+
+            val items = arrayListOf<ArrayList<Int>>()
+            repeat (dimensions[0]) {
+                val row = toArrayList(toIntList(readLine()!!))
+                assert(row.size == dimensions[1])
+
+                items.add(row)
+            }
+            return Matrix(items)
+        }
+    }
+}
+
+
+fun main() {
+    val a = Matrix.readFromStdin()
+    val b = Matrix.readFromStdin()
+
+    try {
+        val sum = a + b
+        println(sum)
+    } catch (_: java.lang.Exception) {
+        println("ERROR")
+    }
+}
