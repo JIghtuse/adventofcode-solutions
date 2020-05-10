8aacd40254cca6e0d8eb065b75c53fd70ed1ef0f
Егоров Борис Владимирович <b.egorov@2gis.ru>
2020-05-10 13:27:44 +0700

    Greatly simplify code with iterators and lambdas

--- src/processor/Main.kt
+++ src/processor/Main.kt
@@ -4,17 +4,7 @@ fun toIntList(s: String): List<Int> {
     return s.split(" ").filter { it.isNotEmpty() }.map { it.toInt() }
 }
 
-fun toArrayList(xs: List<Int>): ArrayList<Int> {
-    val arrayList = arrayListOf<Int>()
-
-    for (item in xs) {
-        arrayList.add(item)
-    }
-
-    return arrayList
-}
-
-class Matrix(val items: ArrayList<ArrayList<Int>>) {
+class Matrix(val items: Array<List<Int>>) {
     val rows: Int = items.size
     val columns: Int = if (items.isEmpty()) 0 else items[0].size
 
@@ -22,55 +12,25 @@ class Matrix(val items: ArrayList<ArrayList<Int>>) {
         if (rows != other.rows) throw Exception("Could not sum matrix of different dimensions (rows)")
         if (columns != other.columns) throw Exception("Could not sum matrix of different dimensions (columns)")
 
-        val sumMatrixItems = arrayListOf<ArrayList<Int>>()
-        repeat(rows) {
-            val row = arrayListOf<Int>()
-            repeat(columns) { jt ->
-                row.add(getItem(it, jt) + other.getItem(it, jt))
-            }
-            sumMatrixItems.add(row)
-        }
-
-        return Matrix(sumMatrixItems)
+        return Matrix(Array(rows) { items[it].zip(other.items[it]) { x, y -> x + y } })
     }
 
     operator fun times(scalar: Int): Matrix {
-        val newItems = arrayListOf<ArrayList<Int>>()
-        for (row in items) {
-            val newRow = arrayListOf<Int>()
-            row.forEach { newRow.add(it * scalar) }
-            newItems.add(newRow)
-        }
-        return Matrix(newItems)
+        return Matrix(Array(rows) { i -> List(columns) { j -> items[i][j] * scalar } })
     }
 
     override fun toString(): String {
         return items.joinToString("\n") { it.joinToString(" ") }
     }
 
-    private fun getItem(row: Int, col: Int): Int {
-        if (row >= rows) throw Exception("Asked for row $row, matrix has only $rows rows.")
-        if (col >= columns) throw Exception("Asked for column $col, matrix has only $columns columns.")
-        return items[row][col]
-    }
-
     companion object {
         fun readFromStdin(): Matrix {
-            val dimensions = toIntList(readLine()!!)
-            assert(dimensions.size == 2)
+            val (rows, columns) = toIntList(readLine()!!)
 
-            val items = arrayListOf<ArrayList<Int>>()
-            repeat (dimensions[0]) {
-                val row = toArrayList(toIntList(readLine()!!))
-                assert(row.size == dimensions[1])
-
-                items.add(row)
-            }
-            return Matrix(items)
+            return Matrix(Array(rows) { toIntList(readLine()!!) })
         }
     }
 }
-
 
 fun main() {
     val a = Matrix.readFromStdin()
