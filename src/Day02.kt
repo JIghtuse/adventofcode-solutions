993ff679d9445a1bdbd8172bb619cf863e27c310
Егоров Борис Владимирович <b.egorov@2gis.ru>
2022-12-01 23:44:55 +0600

    Prepared template for Day 2

---
+++ src/Day02.kt
@@ -0,0 +1,17 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day02_test")
+    check(part1(testInput) == 0)
+
+    val input = readInput("Day02")
+    println(part1(input))
+    println(part2(input))
+}
