832142d6af8478ca8152f57e087c8c0c33252513
Егоров Борис Владимирович <b.egorov@2gis.ru>
2022-12-04 13:33:26 +0600

    Completed day 04: clean up all the rooms

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,15 +1,35 @@
+import java.lang.IllegalArgumentException
+
 fun main() {
+    val inputLineRegex = """(\d+)-(\d+),(\d+)-(\d+)""".toRegex()
+
+    fun parseRanges(line: String): Pair<IntRange, IntRange> {
+        val (aStart, aEnd, bStart, bEnd) = inputLineRegex
+            .matchEntire(line)
+            ?.destructured
+                ?: throw IllegalArgumentException("Incorrect input line $line")
+
+        val a = (aStart.toInt()..aEnd.toInt())
+        val b = (bStart.toInt()..bEnd.toInt())
+        return a to b
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map(::parseRanges).filter { (a, b) ->
+            a.all { it in b } || b.all { it in a }
+        }.size
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.map(::parseRanges).filter { (a, b) ->
+            a.any { it in b } || b.any { it in a }
+        }.size
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day04_test")
-    check(part1(testInput) == 0)
+    check(part1(testInput) == 2)
+    check(part2(testInput) == 4)
 
     val input = readInput("Day04")
     println(part1(input))
