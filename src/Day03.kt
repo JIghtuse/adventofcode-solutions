ace78af9e1421e8441ed8658586a4aa38e2835e4
Егоров Борис Владимирович <b.egorov@2gis.ru>
2023-12-04 21:29:58 +0600

    Day 3 some renaming

--- src/Day03.kt
+++ src/Day03.kt
@@ -46,12 +46,9 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        val yRange = input.indices
-        val xRange = input[0].indices
-
-        fun expandRangeLeft(line: String, range: IntRange): IntRange {
-            var expandedRange = range
-            for (i in range.first downTo 0) {
+        fun expandRangeLeft(line: String, xRange: IntRange): IntRange {
+            var expandedRange = xRange
+            for (i in xRange.first downTo 0) {
                 if (!line[i].isDigit()) break
 
                 expandedRange = i..expandedRange.last
@@ -59,9 +56,9 @@ fun main() {
             return expandedRange
         }
 
-        fun expandRangeRight(line: String, range: IntRange): IntRange {
-            var expandedRange = range
-            for (j in range.last..xRange.last) {
+        fun expandRangeRight(line: String, xRange: IntRange): IntRange {
+            var expandedRange = xRange
+            for (j in xRange.last..input[0].lastIndex) {
                 if (!line[j].isDigit()) break
 
                 expandedRange = expandedRange.first..j
@@ -69,38 +66,38 @@ fun main() {
             return expandedRange
         }
 
-        fun expandRange(line: String, range: IntRange) = expandRangeRight(line, expandRangeLeft(line, range))
+        fun expandRange(line: String, xRange: IntRange) = expandRangeRight(line, expandRangeLeft(line, xRange))
 
-        fun findAdjacentNumbersTopOrBottom(y: Int, range: IntRange): NumberRanges {
+        fun findAdjacentNumbersTopOrBottom(y: Int, xRange: IntRange): NumberRanges {
             val line = input[y]
-            val s = line.subSequence(range)
+            val s = line.subSequence(xRange)
 
             return when {
                 // "..."
                 s.none(Char::isDigit) -> listOf()
                 // "123"
-                s.all(Char::isDigit) -> listOf(y to expandRange(line, range))
+                s.all(Char::isDigit) -> listOf(y to expandRange(line, xRange))
 
                 // ".5", "#2" etc
-                s.length == 2 && s.last().isDigit() -> listOf(y to expandRangeRight(line, range.last..range.last))
+                s.length == 2 && s.last().isDigit() -> listOf(y to expandRangeRight(line, xRange.last..xRange.last))
                 // "5.", "2@" etc
-                s.length == 2 && s.first().isDigit() -> listOf(y to expandRangeLeft(line, range.first..range.first))
+                s.length == 2 && s.first().isDigit() -> listOf(y to expandRangeLeft(line, xRange.first..xRange.first))
 
                 // "23."
-                s[1].isDigit() && s[0].isDigit() -> listOf(y to expandRangeLeft(line, range.first..<range.last))
+                s[1].isDigit() && s[0].isDigit() -> listOf(y to expandRangeLeft(line, xRange.first..<xRange.last))
                 // "@12"
-                s[1].isDigit() && s[2].isDigit() -> listOf(y to expandRangeRight(line, range.first + 1..range.last))
+                s[1].isDigit() && s[2].isDigit() -> listOf(y to expandRangeRight(line, xRange.first + 1..xRange.last))
                 // ".5."
-                s[1].isDigit() -> listOf(y to range.first + 1..range.first + 1)
+                s[1].isDigit() -> listOf(y to xRange.first + 1..xRange.first + 1)
 
                 // "1.5"
                 s[0].isDigit() && s[2].isDigit() -> listOf(
-                    y to expandRangeLeft(line, range.first..range.first),
-                    y to expandRangeRight(line, range.last..range.last))
+                    y to expandRangeLeft(line, xRange.first..xRange.first),
+                    y to expandRangeRight(line, xRange.last..xRange.last))
                 // "2.."
-                s[0].isDigit() -> listOf(y to expandRangeLeft(line, range.first..range.first))
+                s[0].isDigit() -> listOf(y to expandRangeLeft(line, xRange.first..xRange.first))
                 // "..3"
-                s[2].isDigit() -> listOf(y to expandRangeRight(line, range.last..range.last))
+                s[2].isDigit() -> listOf(y to expandRangeRight(line, xRange.last..xRange.last))
 
                 // satisfying compiler with necessary branch
                 else -> listOf()
@@ -109,7 +106,7 @@ fun main() {
 
         fun buildTopOrBottomXRange(x: Int): IntRange {
             val start = maxOf(0, x - 1)
-            val end = minOf(xRange.last, x + 1)
+            val end = minOf(input[0].lastIndex, x + 1)
 
             return start..end
         }
@@ -118,18 +115,18 @@ fun main() {
             val numbers = mutableListOf<Pair<Int, IntRange>>()
 
             // left and right are somewhat trivial: expand to one side
-            if (x - 1 in xRange && input[y][x - 1].isDigit()) {
+            if (x - 1 in input[0].indices && input[y][x - 1].isDigit()) {
                 numbers.add(y to expandRangeLeft(input[y], x - 1..<x))
             }
-            if (x + 1 in xRange && input[y][x + 1].isDigit()) {
+            if (x + 1 in input[0].indices && input[y][x + 1].isDigit()) {
                 numbers.add(y to expandRangeRight(input[y], x + 1..x + 1))
             }
 
             // top and bottom has a few edge cases
-            if (y - 1 in yRange) {
+            if (y - 1 in input.indices) {
                 numbers.addAll(findAdjacentNumbersTopOrBottom(y - 1, buildTopOrBottomXRange(x)))
             }
-            if (y + 1 in yRange) {
+            if (y + 1 in input.indices) {
                 numbers.addAll(findAdjacentNumbersTopOrBottom(y + 1, buildTopOrBottomXRange(x)))
             }
 
