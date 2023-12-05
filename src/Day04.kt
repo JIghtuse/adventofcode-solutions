254e4e1a1c6988d2bc5fa930ac02265949b21c1a
Егоров Борис Владимирович <b.egorov@2gis.ru>
2023-12-05 22:39:43 +0600

    Day 4 solution

---
+++ src/Day04.kt
@@ -0,0 +1,51 @@
+import kotlin.math.pow
+
+fun numberOfMatches(line: String): Int {
+    val numberParts = line.substringAfter(": ")
+    val winningNumbers = numberParts
+        .substringBefore(" | ")
+        .split(" ")
+        .filter(String::isNotEmpty)
+        .map(String::toInt)
+        .toSet()
+    val ourNumbers = numberParts
+        .substringAfter(" | ")
+        .split(" ")
+        .filter(String::isNotEmpty)
+        .map(String::toInt)
+
+    winningNumbers to ourNumbers
+
+    return ourNumbers.count { it in winningNumbers }
+}
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.sumOf { line ->
+            2.0.pow(numberOfMatches(line) - 1).toInt()
+        }
+    }
+
+    fun part2(input: List<String>): Int {
+        val quantities = MutableList(input.size) { 1 }
+
+        for (i in quantities.indices) {
+            val matches = numberOfMatches(input[i])
+
+            for (j in 1..matches) {
+                quantities[i + j] += quantities[i]
+            }
+        }
+
+        return quantities.sum()
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day04_test")
+    check(part1(testInput) == 13)
+    check(part2(testInput) == 30)
+
+    val input = readInput("Day04")
+    part1(input).println()
+    part2(input).println()
+}
